aa8db933db5372ec6598752cc15a1eaefd532fbe
rorcores <orbular@gmail.com>
2023-12-26 10:58:30 -0800

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,25 @@
-import type { Metadata } from 'next'
-import React from 'react'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import React from 'react';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Airlock',
-  description: 'SMS 2FA anywhere. Even on a plane.'
-}
+  description: 'SMS 2FA anywhere. Even on a plane.',
+};
 
-export default function RootLayout ({
-  children
+export default function RootLayout({
+  children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
-  )
+  );
 }
